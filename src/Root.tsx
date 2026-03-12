5ceb6c6f66fea3f14a20d038d4c4bcdd5fbecf6d
tangyuan <18200585623@163.com>
2026-03-12 10:06:43 +0800

    修改将google key换成官方的

--- src/Root.tsx
+++ src/Root.tsx
@@ -2,6 +2,7 @@ import "./index.css";
 import type { FC } from "react";
 import { Composition } from "remotion";
 import { guoyanMd001AELaunchComposition } from "./components/guoyanMd001AELaunch";
+import { guoyanMd001CineFlowComposition } from "./components/guoyanMd001CineFlow";
 import { guoyanMd001DirectorialComposition } from "./components/guoyanMd001Directorial";
 import { guoyanMd001DirectorCutComposition } from "./components/guoyanMd001DirectorCut";
 import { guoyanMd001PromoComposition } from "./components/guoyanMd001Promo";
@@ -12,6 +13,7 @@ export const RemotionRoot: FC = () => {
   return (
     <>
       <Composition {...guoyanMd001AELaunchComposition} />
+      <Composition {...guoyanMd001CineFlowComposition} />
       <Composition {...guoyanMd001DirectorialComposition} />
       <Composition {...guoyanMd001DirectorCutComposition} />
       <Composition {...guoyanMd001PromoComposition} />
